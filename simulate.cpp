1a71a77c636a5576ab69beeda9b4a8642e1ffd42
geckobike <geckobike@github.com>
2016-11-22 23:00:07 +0000

    various testing and tweaks

--- simulate.cpp
+++ simulate.cpp
@@ -26,11 +26,10 @@ static void Zero(void* pointer, int size)
 	memset(pointer, 0, size);
 }
 
-float sqr(float x)
-{
-	return x*x;
-}
-		
+double sqr(double x) { return x*x; }
+double min(double x, double y) { return (x<y) ? x : y; }
+double max(double x, double y) { return (x>y) ? x : y; }
+
 	
 // helper enum for indexing
 enum
@@ -42,15 +41,15 @@ enum
 // Input profile for a simulation
 struct InputProfile
 {
-	float numberOfCyclists;            // Total number of cyclists for this simulation (as a float for convenience)
-	float helmetEffectiveness;         // 0.0 -> 1.0 what fraction of fatal head injuries are prevented by wearing a helmet 
-	float helmetWearingFraction;       // The fraction of cyclists wearing a helmet (0.0 -> 1.0)
+	double numberOfCyclists;            // Total number of cyclists for this simulation (as a double for convenience)
+	double helmetEffectiveness;         // 0.0 -> 1.0 what fraction of fatal head injuries are prevented by wearing a helmet 
+	double helmetWearingFraction;       // The fraction of cyclists wearing a helmet (0.0 -> 1.0)
 
-	float probOfCrashing[2];           // Probability of crashing, for non wearing and helmet wearing cyclist
-	float probFatalHeadInjury[2];      // Probability of this crash becoming a fatal head injury (NB: this is the probability *after crashing*)
-	float probFatalOtherInjury[2];     // Probability of this crash becoming a fatal body injury (after crashing)
+	double probOfCrashing[2];           // Probability of crashing, for non wearing and helmet wearing cyclist
+	double probFatalHeadInjury[2];      // Probability of this crash becoming a fatal head injury (NB: this is the probability *after crashing*)
+	double probFatalOtherInjury[2];     // Probability of this crash becoming a fatal body injury (after crashing)
 
-	float coronerAccuracy;             // When deciding whether a death is a "control" or a "case" how accurate is the coroner? 0->1.0, with 1.0 == 100% accurate
+	double coronerAccuracy;             // When deciding whether a death is a "control" or a "case" how accurate is the coroner? 0->1.0, with 1.0 == 100% accurate
 
 	int repeatCount;                   // How many times to repeat this simulation run
 
@@ -66,7 +65,7 @@ struct Death
 	
 struct ReportedGroup
 {
-	float count[2];         // index 0 - no helmet, index 1 - with helmet
+	double count[2];         // index 0 - no helmet, index 1 - with helmet
 
 	ReportedGroup()
 	{
@@ -118,11 +117,26 @@ public:
 		//fflush(file);
 	}
 
-	float a,b;
-	float c,d;
-	float oddsRatio;
-	float helmetWearingFractionCases;
-	float helmetWearingFractionControls;
+	double a,b;
+	double c,d;
+	double oddsRatio;
+	double helmetWearingFractionCases;
+	double helmetWearingFractionControls;
+};
+
+void PrintProfile(FILE* file, const InputProfile& profile)
+{
+	fprintf(file, "numberOfCyclists %f\n", profile.numberOfCyclists);
+	fprintf(file, "helmetEffectiveness = %f\n", profile.helmetEffectiveness);
+	fprintf(file, "helmetWearingFraction = %f\n", profile.helmetWearingFraction);
+	for (int i=0; i<2; i++)
+	{
+		fprintf(file, "probOfCrashing[%d] = %f\n", i, profile.probOfCrashing[i]);
+		fprintf(file, "probFatalHeadInjury[%d] = %f\n", i, profile.probFatalHeadInjury[i]);
+		fprintf(file, "probFatalOtherInjury[%d] = %f\n", i, profile.probFatalOtherInjury[i]);
+	}
+	fprintf(file, "coronerAccuracy = %f\n", profile.coronerAccuracy);
+	fprintf(file, "repeatCount = %d\n", profile.repeatCount);
 };
 
 void PrintResults(const InputProfile& profile, const ReportedGroup& cases, const ReportedGroup& controls)
@@ -132,8 +146,8 @@ void PrintResults(const InputProfile& profile, const ReportedGroup& cases, const
 	// helmet     a      b
 	// no-helmet  c      d
 	///////////////////////////
-	float a = cases.count[kHelmet],   b = controls.count[kHelmet];
-	float c = cases.count[kNoHelmet], d = controls.count[kNoHelmet];
+	double a = cases.count[kHelmet],   b = controls.count[kHelmet];
+	double c = cases.count[kNoHelmet], d = controls.count[kNoHelmet];
 
 	printf("# =========================================================\n");
 	printf("#  RESULTS: Coroner is %.1f accurate in identifying\n", profile.coronerAccuracy*100.0);
@@ -144,22 +158,22 @@ void PrintResults(const InputProfile& profile, const ReportedGroup& cases, const
 	printf("#   total      %.1f       %.1f\n", a+c, b+d);
 
 	// Proportion of people wearing helmets in the control group
-	float controlHelmetWearingRate = b / (b+d);
+	double controlHelmetWearingRate = b / (b+d);
 
 	// Wearing odds of the control group
-	float controlHelmetWearingOdds = b / d;
+	double controlHelmetWearingOdds = b / d;
 	
 	// Wearing odds of the case group
-	float casesHelmetWearingOdds = a / c;
+	double casesHelmetWearingOdds = a / c;
 
 	// Finally the ODDS-RATIO
-	float oddsRatio = casesHelmetWearingOdds / controlHelmetWearingOdds;
+	double oddsRatio = casesHelmetWearingOdds / controlHelmetWearingOdds;
 
 	printf("\n#  controlHelmetWearingRate = %.1f %\n\n", controlHelmetWearingRate*100.0);
 
 	printf("#  ODDS-RATIO = %f\n\n", oddsRatio);
 	
-	float f = controlHelmetWearingRate;
+	double f = controlHelmetWearingRate;
 
 	oddsRatio = (a+b)/(c+d) * (1-f)/f;
 	printf("#  OVERALL ODDS-RATIO = %f\n\n", oddsRatio);
@@ -196,7 +210,7 @@ void Simulate(const InputProfile& profile, ReportedGroup& outputCases, ReportedG
 			if (g_random() < profile.probOfCrashing[helmet])
 			{
 				// What's the probability of this crash leading to a fatal head injury
-				float probabilityOfFatalHeadInjury = profile.probFatalHeadInjury[helmet];
+				double probabilityOfFatalHeadInjury = profile.probFatalHeadInjury[helmet];
 				// If helemt wearing that probability is reduced by (1-protection)
 				if (helmet)
 					probabilityOfFatalHeadInjury *= 1.0 - profile.helmetEffectiveness;
@@ -272,36 +286,46 @@ int main()
 
 	// See initial notes at the top, the approximate odds a cyclist
 	// getting a fatal head injury in a year is around:
-	const float approximateRiskOfFatalHeadInjury = 1.0/0.9e6;
+	const double approximateRiskOfFatalHeadInjury = 1.0/0.35e6;
 
 	/////////////////////////////////////
 	// Input profile
 	/////////////////////////////////////
 	InputProfile profile;
 	profile.numberOfCyclists = 4e6;        // around 13.6 million ontarians, ~30% cycle weekly
-	profile.helmetEffectiveness = 0.5;     // pick a random number :-/
+	profile.helmetEffectiveness = 0.3;     // pick a random number :-/
 	profile.helmetWearingFraction = 0.36;  // 2009 survey ~ 36% cyclists wearing a helmet
    	// No idea what the probability of crashing should be so choose something "sensible"
 	// and then scale the risk of a subsequent fatal head injury using the overall approximateRiskOfFatalHeadInjury
-	profile.probOfCrashing[kNoHelmet] = 1.0/10000.0;
+	profile.probOfCrashing[kNoHelmet] = 1.0/100.0;
 	profile.probFatalHeadInjury[kNoHelmet] = approximateRiskOfFatalHeadInjury / profile.probOfCrashing[kNoHelmet];
 	// From persaud it looked for every fatal head injury there were around
 	profile.probFatalOtherInjury[kNoHelmet] = profile.probFatalHeadInjury[kNoHelmet] * 0.7f;
 	// Does helmet wearing population have more chance of crashing
 	// and a higher injury risk?
+
 	profile.probOfCrashing[kHelmet] = profile.probOfCrashing[kNoHelmet];
 	profile.probFatalHeadInjury[kHelmet] = profile.probFatalHeadInjury[kNoHelmet];
 	profile.probFatalOtherInjury[kHelmet] = profile.probFatalOtherInjury[kNoHelmet];
+
+	// Optimisatio so the simulation runs faster	
+	double hack = max(profile.probOfCrashing[kNoHelmet], profile.probOfCrashing[kHelmet]);
+	profile.numberOfCyclists *= hack;
+	profile.probOfCrashing[kHelmet] *= 1.0/hack;
+	profile.probOfCrashing[kNoHelmet] *= 1.0/hack;
 
 	// How accurately does the coroner assess and classigy the controls:
 	profile.coronerAccuracy = 1.0;
 	profile.repeatCount = 5;               // repeat this 5 times, as Persaud et al results were from 2006-2010, spanning 5 years
 	profile.optimisationReserveCount = 500;
 
+	//PrintProfile(stderr, profile);
+	//exit(0);
+
 	///////////////////////////////////////
 	// Run the simulation
 	///////////////////////////////////////
-	const int numIterations = 100;
+	const int numIterations = 20000;
 	OddsRatio* results = new OddsRatio[numIterations];
 
 	for (int i=0; i<numIterations; i++)
@@ -310,7 +334,13 @@ int main()
 		ReportedGroup controls;
 		Simulate(profile, cases, controls);
 		results[i].Calculate(cases, controls);
-		//results[i].Print(stdout);
+		//results[i].Print(stderr);
+		//if (i>10) exit(0);
+		if ((i%100)==0)
+		{
+			fprintf(stderr, "%d of %d\n", i, numIterations);
+			fflush(stderr);
+		}
 	}
 
 	///////////////////////////////////////
@@ -318,25 +348,46 @@ int main()
 	///////////////////////////////////////
 	
 	// Mean
-	float mean = 0.0;
+	double mean = 0.0;
 	for (int i=0; i<numIterations; i++)
 	{
 		mean += results[i].oddsRatio;
 	}
-	mean /= (float)numIterations;
+	mean /= (double)numIterations;
 	
 	// Variance
-	float variance = 0.0;
+	double variance = 0.0;
 	for (int i=0; i<numIterations; i++)
 	{
-		float x = results[i].oddsRatio;
+		double x = results[i].oddsRatio;
 		variance += sqr(x - mean);
 	}
-	variance /= (float)(numIterations-1);
+	variance /= (double)(numIterations-1);
 
-	float standardDeviation = sqrt(variance);
+	double standardDeviation = sqrt(variance);
 
-	printf("%f, %f, %f\n", mean, variance, standardDeviation);
+	printf("# # # %f, %f, %f\n", mean, variance, standardDeviation, 2.0*standardDeviation);
+
+	// Create a histogram
+	double resolution = max(0.001, min(0.05, standardDeviation/5.0));
+	int numBuckets = (int)((3.0+resolution)/resolution);
+	double* buckets = new double[numBuckets];
+	Zero(buckets, sizeof(double)*numBuckets);
+	
+	for (int i=0; i<numIterations; i++)
+	{
+		int bucket = (int)(((results[i].oddsRatio)/resolution)+0.5f);
+		if (bucket>=0 && bucket<numBuckets)
+		{
+			buckets[bucket] += 1.0;
+		}
+	}
+
+	// Output the histogram
+	for (int i=0; i<numBuckets; i++)
+	{
+		printf("%f %f\n", (double)i*resolution, buckets[i]);
+	}
 	
 	// Clean up
 	delete [] results;
